f336ab8705171cc19600816a6c011d0a41e25ef2
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-03-26 21:17:45 -0300

    refactor(CinemaLaPlata): long live dicts

--- src/cinema.py
+++ src/cinema.py
@@ -2,22 +2,6 @@ from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup
 import requests
 
-class Movie:
-    """A movie..."""
-
-    # def __init__(self, properties):
-    #     self.title = properties[title]
-    #     self.genre = genre
-    #     self.language = language
-    #     self.origin = origin
-    #     self.web = web
-    #     self.duration = duration
-    #     self.director = director
-    #     self.rating = rating
-    #     self.cast = cast
-    #     self.synopsis = synopsis
-    #     self.schedule = schedule
-
 
 class CinemaScrappy(ABC):
     """Abstract scrapper for cinema pages
@@ -36,12 +20,12 @@ class CinemaScrappy(ABC):
 
         Returns:
             BeatifulSoup: HTML form page to scrap
-        """        
+        """
 
         return BeautifulSoup(requests.get(link).text, "html.parser")
 
     @abstractmethod
-    def scrap(self):
+    def scrape(self):
         """Fetch movies' info in _cinema_page, return a list """
 
         return
@@ -50,38 +34,88 @@ class CinemaScrappy(ABC):
 class CinemaLaPlataScrappy(CinemaScrappy):
     """Scrapper of Cinema La Plata's billboard"""
 
-    def __init__(self):        
+    def __init__(self):
         super().__init__(_cinema_page="http://www.cinemalaplata.com/")
 
+    def _scrape_movie_title(self, movie_container):
+        return (
+            movie_container.find("div", class_="page-title").get_text().strip()
+        )
+
+    def _scrape_movie_traits(self, movie_container):
+        return {
+            trait.find("h4")
+            .get_text()
+            .strip(): trait.find("span")
+            .get_text()
+            .strip()
+            for trait in movie_container.find_all(
+                "div", attrs={"class": "dropcap6"}
+            )
+        }
+
+    def _scrape_movie_schedule(self, movie_container):
+        # {
+        #   movie 1 = {
+        #     horarios = {
+        #       cine 1 =
+        #       cine 2 =
+        #       cine 3 =
+        #     }
+        #   }
+        # }
+
+        schedule_container = movie_container.find(
+            "div", attrs={"id": "ctl00_cph_pnFunciones"}
+        )
+
+        # movie_schedule_children = movie_schedule.findChildren()
+        # movie_data[movie_schedule_children[0].get_text().strip()] = {}
+        # del movie_schedule_children[0]
+        # for schedule in movie_schedule_children:
+        #     pass
+        horarios = {
+            cinema.find("span")
+            .get_text()
+            .strip(): cinema.find("p")
+            .get_text()
+            .strip()
+            .split(r"\n")
+            for cinema in movie_container.find_all(
+                "div", attrs={"class": "col-2"}
+            )
+        }
+
+        return {schedule_container.find("h4").get_text().strip(): horarios}
+
     def _scrape_movie(self, movie_link):
-        # Declaracion de un diccionario vacio con los datos de una pelicula
-        movie_data = {}
-        
         # Tomamos los datos de la pelicula
         movie_container = self._make_soup(self._cinema_page + movie_link)
-        movie_data["title"] = movie_container.find(id="ctl00_pnTitu").get_text().strip()
-        for data in movie_container.find_all("div", attrs={"class":"dropcap6"}):
-            movie_data[data.contents[1].get_text().strip()] = data.contents[3].get_text().strip()
-        
-        # Tomamos horario y cine donde esta la pelicula
-        movie_schedule = movie_container.find("div", attrs={"id":"ctl00_cph_pnFunciones"})
-        movie_schedule_children = movie_schedule.findChildren()
-        movie_data[movie_schedule_children[0].get_text().strip()] = {}
-        del movie_schedule_children[0]
-        for schedule in movie_schedule_children:
-            pass
-        return movie_data
 
-    def scrap(self):
+        movie = self._scrape_movie_traits(movie_container)
+
+        movie["Sinopsis"] = (
+            movie_container.find(id="ctl00_cph_lblSinopsis").get_text().strip()
+        )
+
+        movie.update(self._scrape_movie_schedule(movie_container))
+
+        return {self._scrape_movie_title(movie_container): movie}
+
+    def scrape(self):
         """
-        Method that extracts the information of the Cinemas La Plata billboard
-        """        
+        Extract information from Cinema La Plata's billboard
+        """
         movies_container = self._make_soup(
             self._cinema_page + "Cartelera.aspx"
         ).find_all("div", attrs={"class": "page-container singlepost"})
-        movies_info = {}
-        for movie in movies_container:
-            movie_info = self._scrape_movie(movie.find("a").get("href"))
-            movies_info[movie_info["title"]] = movie_info
 
-CinemaLaPlataScrappy().scrap()
+        movies = {}
+        for movie in movies_container:
+            movies.update(self._scrape_movie(movie.find("a").get("href")))
+            print(movies)
+            return 0
+        return movies
+
+
+CinemaLaPlataScrappy().scrape()
